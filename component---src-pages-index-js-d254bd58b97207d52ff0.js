31bc0655378b1da385d06c6294ec53cf03ad979c
Dmitriy <patrickjsmirnov@gmail.com>
2020-04-18 14:00:02 +0300

    Updates

---
+++ component---src-pages-index-js-d254bd58b97207d52ff0.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{RXBc:function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),i=t.n(a),c=t("vGFT"),o=t.n(c);n.default=function(){return i.a.createElement("div",{className:o.a.main},i.a.createElement("span",{className:o.a.title},"Смирнов Дмитрий"))}},vGFT:function(e,n,t){e.exports={main:"index-module--main--3W71P",title:"index-module--title--2oVtp"}}}]);
+//# sourceMappingURL=component---src-pages-index-js-d254bd58b97207d52ff0.js.map
