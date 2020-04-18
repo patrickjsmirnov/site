240aa8d26e2f815c7da421a3b73f7f9cfaac6d66
Dmitriy <patrickjsmirnov@gmail.com>
2020-04-18 13:35:39 +0300

    initial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
+import mainStyles from "./index.module.css"
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div className={mainStyles.main}>
+    <span className={mainStyles.title}>Смирнов Дмитрий</span>
+  </div>
+)
+
